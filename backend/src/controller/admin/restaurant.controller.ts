aaadab23044057fd8303247f4d89a6fe450bdc70
semilmore <smore@plusonex.com>
2025-01-29 10:48:25 +0530

    Created Put and update status Api for restaurant

--- backend/src/controller/admin/restaurant.controller.ts
+++ backend/src/controller/admin/restaurant.controller.ts
@@ -44,3 +44,58 @@ export const getAllRestaurantsOfUser = expressAsyncHandler(
     }
   },
 );
+
+export const updateRestaurant = expressAsyncHandler(
+  async (req: Request, res: Response) => {
+    try {
+      const restaurantID = req.params?.restaurantId;
+
+      const updateRecord = await Restaurant.findByIdAndUpdate(
+        { _id: restaurantID },
+        req.body,
+        { new: true },
+      );
+
+      if (updateRecord) {
+        res.status(200).send({ response: updateRecord });
+      } else {
+        res.status(400).send({ response: "Failed to update restaurnt record" });
+      }
+
+      res.status(200).send({ response: updateRecord });
+    } catch (error) {
+      res
+        .status(500)
+        .send({ response: "Server Error, failed to update restaurant" });
+    }
+  },
+);
+
+export const updateRestaurantStatus = expressAsyncHandler(
+  async (req: Request, res: Response) => {
+    try {
+      const restaurantID = req?.params?.restaurantId;
+      const { status } = req.body;
+
+      const updatedStatus = status === true ? false : true;
+
+      const updateRecordStatus = await Restaurant.findByIdAndUpdate(
+        { _id: restaurantID },
+        { status: updatedStatus },
+        { new: true },
+      );
+
+      if (updateRecordStatus) {
+        res.status(200).send({ response: updateRecordStatus?.status });
+      } else {
+        res
+          .status(400)
+          .send({ response: "Failed to update restaurant status" });
+      }
+    } catch (error) {
+      res
+        .status(500)
+        .send({ response: "Server error, failed to update restaurnt status" });
+    }
+  },
+);
--- backend/src/route/admin/restaurant.route.ts
+++ backend/src/route/admin/restaurant.route.ts
@@ -1,6 +1,8 @@
 import {
   addNewRestaurant,
   getAllRestaurantsOfUser,
+  updateRestaurant,
+  updateRestaurantStatus,
 } from "../../controller/admin/restaurant.controller";
 import express from "express";
 import verifyTokenExists from "../../middleware/verify-token.middleware";
@@ -9,5 +11,16 @@ const restaurantRouter = express.Router();
 
 restaurantRouter.post("/create", verifyTokenExists, addNewRestaurant);
 restaurantRouter.post("/user", verifyTokenExists, getAllRestaurantsOfUser);
+restaurantRouter.put(
+  "/update/:restaurantId",
+  verifyTokenExists,
+  updateRestaurant,
+);
+
+restaurantRouter.put(
+  "/update-status/:restaurantId",
+  verifyTokenExists,
+  updateRestaurantStatus,
+);
 
 export default restaurantRouter;
